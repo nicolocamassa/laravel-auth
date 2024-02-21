d756ea708394b7d8bd8a0250f687842eed81bec3
nicolocamassa <camassanicolo03@gmail.com>
2024-02-21 16:31:11 +0100

    [UPDATE] seeder complete

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project as Project;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,9 +14,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-
+        for($i = 0; $i < 10; $i++){
+            $newProject = new Project();
+            $newProject->title = $faker->words(3, true);
+            $newProject->description = $faker->sentence();
+            $newProject->date = $faker->date();
+            $newProject->languages = $faker->randomElement(['Italiano', 'Inglese', 'Tedesco', 'Francese']);
+            $newProject->status = $faker->randomElement(['Completato', 'In Corso', 'Iniziale']);
+            $newProject->save();
+        }
 
     }
 }
